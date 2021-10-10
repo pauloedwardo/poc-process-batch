bc24cbc8b2985fb46fb31cd314f897eff7de727b
Paulo Eduardo <plo.erd@gmail.com>
2021-10-10 19:25:12 -0300

    Initial commit.

---
+++ src/main/java/pocprocessbatch/PocProcessBatchApplication.java
@@ -0,0 +1,13 @@
+package pocprocessbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocProcessBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocProcessBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pocprocessbatch/PocProcessBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package pocprocessbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocProcessBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
